ba9c14c312589f960fe43d96af41fa27ef55107b
d3uceY <onyekwelujesse1234@gmail.com>
2026-01-15 19:14:27 +0100

    feat: update app dimensions and background color for improved UI consistency

--- main.go
+++ main.go
@@ -19,13 +19,14 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "Ya GUI",
-		Width:  1024,
-		Height: 768,
+		Title:    "Ya GUI",
+		Width:    800,
+		Height:   600,
+		MaxWidth: 800,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		BackgroundColour: &options.RGBA{R: 220, G: 230, B: 241, A: 255},
 		OnStartup:        app.startup,
 		Bind: []interface{}{
 			app,
